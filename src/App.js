61307a9217ba977b6fcac08106bfa8578a23f3b7
Abdulxd <tartilaci@gmail.com>
2024-02-21 10:23:30 +0100

    add style css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 import React from 'react';
 import Msg from './msg/Msg';
+import Footer from './footer/Footer';
 
 function App () {
   return (
     <div>
     
     <Msg/>
+    <Footer/>
 
 
     </div>
